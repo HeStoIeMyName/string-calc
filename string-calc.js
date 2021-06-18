345d24039e529d44f759dd85d67f2c7bf8359cf5
HeStoIeMyName <bruno-koenig@web.de>
2021-06-18 23:06:43 +0200

    initial setup

---
+++ string-calc.js
@@ -0,0 +1,12 @@
+var comma = ','
+
+function stringCalc(numbers, comma)
+{
+    numbers.split(comma)
+}
+
+function addNumbers() {
+    const sum = stringCalc(numbers, comma)
+
+    console.log("Die Zahl lautet " + sum)
+}
---
+++ string-calc.spec.js
@@ -0,0 +1,7 @@
+describe("stringCalc", () =>{
+    it("should return zero on empty string", () => {
+        console.log("test")
+//        expect(stringCalc(numbersMock)).toEqual("1,2")
+    })
+})
+
